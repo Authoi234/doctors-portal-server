a082350da5cdbe9aaff7057e4132a73ab6fb95b2
Authoi234 <jawadkhan.authoi@gmail.com>
2024-02-10 23:39:09 +0600

    admin is verified at all admin routes and get bookings by id is complete

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ async function run() {
         const doctorsCollection = client.db('doctorsPortal').collection('doctors');
 
         // NOTE: make sure you use verify admin after verify jwt
-        const verifyAdmin = async(req, res, next) => {
+        const verifyAdmin = async (req, res, next) => {
             const decodedEmail = req.decoded.email;
             const query = { email: decodedEmail };
             const user = await usersCollection.findOne(query);
@@ -145,6 +145,13 @@ async function run() {
             res.send(bookings);
         });
 
+        app.get('/bookings/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id : new ObjectId(id)};
+            const booking = await bookingsCollection.findOne(query);
+            res.send(booking);
+        })
+
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
             console.log(booking);
@@ -223,6 +230,19 @@ async function run() {
             const result = await usersCollection.updateOne(filter, updatedDoc, options);
             res.send(result);
         });
+
+        // temporaly to update price field on appoinment options
+        // app.get('/addPrice', async (req, res) => {
+        //     const filter = {};
+        //     const options = { upsert: true }
+        //     const updatedDoc = {
+        //         $set: {
+        //             price: 99
+        //         }
+        //     }
+        //     const result = await appointmentOptionsCollection.updateMany(filter, updatedDoc, options);
+        //     res.send(result);
+        // })
 
         app.get('/doctors', verifyJWT, verifyAdmin, async (req, res) => {
             const query = {};
